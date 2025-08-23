46f8c2655855b55721f83633b7b29c2504df1420
Mahmoud Khaled <maatya@banquemisr.com>
2025-08-23 14:37:37 +0300

    first commit

---
+++ ThmanyahApp/ThmanyahAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThmanyahAppApp.swift
+//  ThmanyahApp
+//
+//  Created by Mahmoud Khaled on 23/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ThmanyahAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
